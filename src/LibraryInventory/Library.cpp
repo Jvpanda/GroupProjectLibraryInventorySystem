d043aafbb423a396cc51c0fdeabd455f21717f87
Jared Masters <mastersjared3@gmail.com>
2025-11-22 04:05:34 -0800

    fix: library swap() function compartment variables fix: library operator[] error handling

--- src/LibraryInventory/Library.cpp
+++ src/LibraryInventory/Library.cpp
@@ -27,7 +27,7 @@ void Library::addItem(Item *item, const int shelfIndex, const int compartmentInd
         {
                 (*this)[shelfIndex][compartmentIndex].addItem(item);
         }
-        catch (std::exception e)
+        catch (const std::exception& e)
         {
                 std::cout << "Compartment Does Not Exist.\n";
         }
@@ -36,38 +36,48 @@ void Library::addItem(Item *item, const int shelfIndex, const int compartmentInd
 void Library::swap(int itemOneShelfIndex, int itemOneCompIndex,
                    int itemTwoShelfIndex, int itemTwoCompIndex)
 {
-        Compartment &compartment1 = (*this)[itemOneShelfIndex][itemOneCompIndex];
-        Compartment &compartment2 = (*this)[itemTwoShelfIndex][itemTwoCompIndex];
+        Compartment *compartment1 = nullptr;
+        Compartment *compartment2 = nullptr;
+
+        try
+        {
+                compartment1 = &(*this)[itemOneShelfIndex][itemOneCompIndex];
+                compartment2 = &(*this)[itemTwoShelfIndex][itemTwoCompIndex];
+        }
+        catch (const std::exception& e)
+        {
+                std::cout << "Compartment not found.\n";
+        }
 
         Item *item1;
         Item *item2;
 
         try
         {
-                item1 = compartment1.removeItem();
-                item2 = compartment2.removeItem();
+                item1 = compartment1->removeItem();
+                item2 = compartment2->removeItem();
         }
-        catch (std::exception e)
+        catch (const std::exception& e)
         {
                 std::cout << "One of the items do not exist.\n";
                 return;
         }
 
-        compartment1.setHasOwner(false);
-        compartment2.setHasOwner(false);
+        compartment1->setHasOwner(false);
+        compartment2->setHasOwner(false);
 
-        compartment1.addItem(item2);
-        compartment2.addItem(item1);
+        compartment1->addItem(item2);
+        compartment2->addItem(item1);
 }
 
 Shelf& Library::operator[](int index)
 {
-        try
+        if (index < 0 || index >= shelves.size())
+        {
+                throw ShelfNotFoundException("Shelf does not exist.");
+        }
+        else
         {
                 return shelves[index];
-        }
-        catch (std::exception e)
-        {
-                std::cout << "Shelf Does Not Exist.\n";
         }
 }
